e036ecac9bffe899eeedef5b418b3417b764b925
rmit-s3598797-yunfei-zhou <31241547+rmit-s3598797-yunfei-zhou@users.noreply.github.com>
2018-05-17 23:15:26 +1000

    Add files via upload

---
+++ Relation.java
@@ -0,0 +1,9 @@
+//developer: YunFei Zhou s3598797
+package interfaces;
+
+public interface Relation {
+	public String showRelation(String name1,String name2);
+	public String getName1();
+	public String getName2();
+	public String getRelation();
+}
